ea0841942d553e08e9d6936696eb3c7be5c8dafd
Daniel <daniel@Daniels-MBP.attlocal.net>
2024-09-25 00:53:58 -0400

    deploy

--- Model/taskmodel.js
+++ Model/taskmodel.js
@@ -4,11 +4,15 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     title: String,
     content: String,
+    createdAt: {
+        type: Date,
+        default: Date.now()
+
+    },
     user: {
         type:mongoose.Schema.ObjectId,
         // ref: 'accounts',
         required: true
-    }
-})
+    }})
 
 module.exports = mongoose.model('taskdb',TaskSchema )
--- Routes/TaskRoute.js
+++ Routes/TaskRoute.js
@@ -12,37 +12,57 @@ TaskRouter.use(cors({
 }));
 TaskRouter.use(express.json());
 TaskRouter.use(cookieParser())
-TaskRouter.use(express.urlencoded({extended: true}));
+TaskRouter.use(express.urlencoded({ extended: true }));
 
 
-TaskRouter.get('/', verifyToken, ((req,res)=>{
+TaskRouter.get('/', verifyToken, ((req, res) => {
     res.send('task router')
-    
+
 }))
 
 
-TaskRouter.post('/addtask', verifyToken,(async(req,res)=>{
-    const {title, content} = req.body
-    const createTask =  await taskmodel.create({
-        title, 
+TaskRouter.post('/addtask', verifyToken, (async (req, res) => {
+    const { title, content } = req.body
+    const createTask = await taskmodel.create({
+        title,
         content,
         user: req.user.id
     })
     await createTask.save()
-    .then((result)=>{
-        if(!result) return res.status(400).json({message: 'Err creating Task...'})
-            res.status(200).json({message: 'task ctreated', result})
-            
-    })
-    
+        .then((result) => {
+            if (!result) return res.status(400).json({ message: 'Err creating Task...' })
+            res.status(200).json({ message: 'task ctreated', result })
+
+        })
+
 }))
 
-TaskRouter.get('/getTasks', verifyToken, ((req,res)=>{
-    taskmodel.find({user: req.user.id}).select('-_id')
-    .then(result=> res.status(200).json({result}))
-    
+TaskRouter.get('/getTasks', verifyToken, ((req, res) => {
+    taskmodel.find({ user: req.user.id })
+        .then(result => res.status(200).json({ result }))
+
 }))
 
+TaskRouter.get('/delete/:id', (async (req, res) => {
+    const {id }= req.params
+    await taskmodel.findByIdAndDelete(id)
+        .then((deletedTask) => {
+                try{
+                    if (deletedTask) {
+                        res.status(200).json({message:'task deleted successfully', deletedTask});
+                    } else {
+                        console.log('task not found');
+                    }
+                    
+                }catch(err){
+                    console.error('Error deleting task:', err);
+
+                }
+
+        })
+
+
+}))
 
 
 
