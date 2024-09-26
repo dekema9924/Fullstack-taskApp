bac5f2839ef37681b9dcb5e1efa16323b0e1a481
Daniel <daniel@Daniels-MBP.attlocal.net>
2024-09-25 20:15:21 -0400

    cors origin

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const cors = require('cors')
 //ROUTERS
 app.use('/accountroute', accountRoute)
 app.use('/taskroute', TaskRouter)
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:5173/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 
