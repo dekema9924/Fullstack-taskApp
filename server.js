831f841ab40874d52c82902171f62fffdba63056
Daniel <daniel@Daniels-MBP.attlocal.net>
2024-09-25 20:34:16 -0400

    access-control

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ const corsOptions ={
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 
 
